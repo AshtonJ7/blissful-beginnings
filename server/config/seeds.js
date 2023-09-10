3dcbd4c9b387f5a291bbb135f22faaf845ca2479
BondT1 <tombond248@gmail.com>
2023-09-10 18:46:43 +0100

    further seeding data

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -19,21 +19,21 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Sleepsuit',
+      name: 'Sleepsuit 3-pack',
       description:
         'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
-      image: 'cookie-tin.jpg',
+      image: 'sleepsuit-pack.jpg',
       category: categories[0]._id,
-      price: 10.99,
+      price: 14.99,
       quantity: 500
     },
     {
-      name: 'Socks',
+      name: 'Socks 3-pack',
       description:
         'Praesent sed lacinia mauris. Nulla congue nibh magna, at feugiat nunc scelerisque quis. Donec iaculis rutrum vulputate. Suspendisse lectus sem, vulputate ac lectus sed, placerat consequat dui.',
-      image: 'canned-coffee.jpg',
+      image: 'socks-3-pack.jpg',
       category: categories[0]._id,
-      price: 5.99,
+      price: 7.99,
       quantity: 500
     },
     {
@@ -41,7 +41,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Donec volutpat erat erat, sit amet gravida justo sodales in. Phasellus tempus euismod urna. Proin ultrices nisi ut ipsum congue, vitae porttitor libero suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam lacinia a nisi non congue.',
-      image: 'toilet-paper.jpg',
+      image: 'baby-bottle.jpg ',
       price: 4.99,
       quantity: 20
     },
@@ -50,7 +50,7 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'soap.jpg',
+      image: 'pushchair.jpg',
       price: 99.99,
       quantity: 50
     },
@@ -59,17 +59,17 @@ db.once('open', async () => {
       category: categories[1]._id,
       description:
         'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
-      image: 'wooden-spoons.jpg',
+      image: 'baby-powder.jpg',
       price: 7.99,
       quantity: 100
     },
     {
-      name: 'Cot',
+      name: 'Nursery Cot',
       category: categories[2]._id,
       description:
         'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'camera.jpg',
-      price: 30.99,
+      image: 'cot.jpg',
+      price: 35.99,
       quantity: 30
     },
     {
@@ -77,16 +77,16 @@ db.once('open', async () => {
       category: categories[2]._id,
       description:
         'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'tablet.jpg',
+      image: 'drawers.jpg',
       price: 150.99,
       quantity: 30
     },
     {
-      name: 'Word Book',
+      name: 'Baby Educational Book',
       category: categories[3]._id,
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'bedtime-book.jpg',
+      image: 'book.jpg',
       price: 7.99,
       quantity: 1000
     },
@@ -94,35 +94,53 @@ db.once('open', async () => {
       name: 'Abacus',
       category: categories[3]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'spinning-top.jpg',
+      image: 'abacus.jpg',
       price: 1.99,
       quantity: 200
     },
     {
-      name: 'Elephant Plushy',
+      name: 'Elephant Plush',
       category: categories[4]._id,
       description:
         'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'plastic-horses.jpg',
+      image: 'elephant-plush.jpg',
       price: 8.99,
       quantity: 100
     },
     {
-      name: 'Bear Plushy',
+      name: 'Bear Plush',
       category: categories[4]._id,
       description:
         'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'teddy-bear.jpg',
+      image: 'bear-plush.jpg',
       price: 8.99,
       quantity: 100
     },
     {
-      name: 'Rabbit Plushy',
+      name: 'Rabbit Plush',
       category: categories[4]._id,
       description:
         'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'alphabet-blocks.jpg',
+      image: 'rabbit-plush.jpg',
       price: 8.99,
+      quantity: 100
+    },
+    {
+      name: 'Baby Monitor',
+      category: categories[5]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'baby-monitor.jpg',
+      price: 45.99,
+      quantity: 150
+    },
+    {
+      name: 'Thermometer',
+      category: categories[5]._id,
+      description:
+        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
+      image: 'thermometer.jpg',
+      price: 25.99,
       quantity: 100
     }
   ]);
