51f2ad042601ffe62528df7ea929424b09f1c46e
Robyn Smith <robynsmith21@hotmail.com>
2023-09-14 20:09:20 +0100

    added Mo's Carousel to main

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,13 +2,14 @@ import React from "react";
 import ProductList from "../components/ProductList";
 import CategoryMenu from "../components/CategoryMenu";
 import Cart from "../components/Cart";
-// import Landing from "../components/Landing/index";
+import Landing from "../components/Landing/index";
 
 const Home = () => {
   return (
     <div className="container">
       {/* <Landing /> */}
       <CategoryMenu />
+      <Landing/>
       <ProductList />
       <Cart />
     </div>
