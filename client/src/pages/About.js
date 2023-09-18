d2faddd71db8a8c52e1242560a7664c3a147e31c
AshtonJ7 <AshtonJ7@users.noreply.github.com>
2023-09-18 20:05:07 +0100

    stride api key

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -11,11 +11,11 @@ const About = () => {
           Dynamic shop for parents and carers of babies and young children. Offering a wide variety of products intuitive shopping experience.
 <br></br>
 <br></br>
-However, as more and more people are raising concerns over the sustainability ethics of a site like this, we felt that families, parents and carers deserve an ethical, humanistic and efficient place to browse
- potential purchases, without losing out on the benefits and quality of life features that come with massive online shops.
+Sustainability is at the core of our ethics. As more and more people are raising concerns over the ethical practices of a site like this, we felt that families, parents and carers deserve an ethical, humanistic and efficient place to browse
+ potential purchases.
 <br></br>
 <br></br>
-With an AI assistant, robust filters and a clean, lightweight UI, Blissful Beginnings is your one-stop-shop for all your Baby and Child needs.
+Our goal is to provide a seamless and enjoyable shopping experience for all your baby essentials, from adorable clothing to safe and innovative baby gear, Blissful Beginnings is your one-stop-shop for all your Baby needs.
           </span>
         </p>
       {/* </Jumbotron> */}
